e0e7aae05cdc2bbba3cf3b2d9d9248d008f79e51
mizanor21 <mizanorrahman398@gmail.com>
2026-03-14 16:06:24 +0600

    buying house page polishing

--- app/(pages)/buying-house/page.tsx
+++ app/(pages)/buying-house/page.tsx
@@ -40,7 +40,7 @@ export default function BuyingHousePage() {
       />
 
       {/* Product Expertise Section */}
-      <ProductShowcase products={PRODUCT_CATEGORIES} />
+      <ProductShowcase />
 
       <HowWeWorkSection />
 
