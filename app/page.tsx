fd6d11ffdcf17197e8132768a3bea1ca6b191f31
sinu5550 <intisarahmedsiyan@gmail.com>
2026-03-16 11:48:06 +0600

    [solve] merger issue solved

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,7 @@ import {
   VALUES,
   CONTACT_INFO,
 } from "@/lib/constants";
-<<<<<<< HEAD
 import HowWeWork from "@/components/sections/HowWeWork";
-=======
-import ProductsAndServices from "@/components/sections/ProductsAndServices";
-import FactoryAndMachinery from "@/components/sections/FactoryAndMachinery";
->>>>>>> a21a59ebcaa1e8da217eca258ab0ad5790eead96
 import { getBannerSlides } from "@/lib/banner";
 import { getWeWorks } from "@/lib/weWork";
 import HowWeWorkSection from "@/components/sections/HowWeWorkSection";
